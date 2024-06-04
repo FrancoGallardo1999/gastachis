b94457f112d3889c4a7bd3ef40d45adcfc40e290
FrancoGallardo1999 <167932980+FrancoGallardo1999@users.noreply.github.com>
2024-06-03 21:54:49 -0300

    Add files via upload

---
+++ gastachis.py
@@ -0,0 +1,8 @@
+gastos = float(input("Cuanto gastaron los gachis? "))
+gachi = float(gastos * 0.57)
+tachi = float(gastos * 0.43)
+print ("Los gastos segun el sueldo serian:")
+print ("El gachi tiene que poner:")
+print (f"{gachi:.2f}")
+print ("La tachita tiene que poner:")
+print (f"{tachi:.2f}")
